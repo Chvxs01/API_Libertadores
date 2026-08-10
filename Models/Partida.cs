6c8800077fd6f509e7253f3bcdd2a77ca443e977
Chvxs01 <gchaves961@gmail.com>
2026-08-10 08:32:50 -0300

    Comentário novo

--- Models/Partida.cs
+++ Models/Partida.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//JOGAÇO
+
 namespace ApiLibertadoresHAS.Models
 {
     public class Partida
--- obj/Debug/net10.0/ApiLibertadoresHAS.AssemblyInfo.cs
+++ obj/Debug/net10.0/ApiLibertadoresHAS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03bef96c16b86e47a8e316ab8fa233bfa63f538")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiLibertadoresHAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
